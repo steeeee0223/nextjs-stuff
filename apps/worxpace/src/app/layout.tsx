e8ede8adadb265460e61326b11f9c6bff45187ec
steeeee0223 <mr.steven7@gmail.com>
2024-01-23 08:14:20 +0000

    ✨ `worxpace` Setup project

---
+++ apps/worxpace/src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata, Viewport } from "next";
+
+import { cn } from "@acme/ui/lib";
+
+import { env } from "~/env";
+
+import "~/app/globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    env.VERCEL_ENV === "production"
+      ? "https://turbo.t3.gg"
+      : "http://localhost:3000",
+  ),
+  title: "Create T3 Turbo",
+  description: "Simple monorepo with shared backend for web & mobile apps",
+  openGraph: {
+    title: "Create T3 Turbo",
+    description: "Simple monorepo with shared backend for web & mobile apps",
+    url: "https://create-t3-turbo.vercel.app",
+    siteName: "Create T3 Turbo",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@jullerino",
+    creator: "@jullerino",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans text-foreground antialiased",
+        )}
+      >
+        {props.children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/worxpace/src/app/page.tsx
@@ -0,0 +1,13 @@
+export const runtime = "edge";
+
+export default async function HomePage() {
+  return (
+    <main className="container h-screen py-16">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Create <span className="text-primary">T3</span> Turbo
+        </h1>
+      </div>
+    </main>
+  );
+}
