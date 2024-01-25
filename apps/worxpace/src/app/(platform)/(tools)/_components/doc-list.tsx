d1a3893c73d62ec57fdd6899b078ae6f2428066e
steeeee0223 <mr.steven7@gmail.com>
2024-01-25 07:43:48 +0000

    🚀 `worxpace` 5 - Sidebar ui: nested doc list

--- apps/worxpace/src/app/(platform)/(tools)/_components/doc-list.tsx
+++ apps/worxpace/src/app/(platform)/(tools)/_components/doc-list.tsx
@@ -1,10 +1,42 @@
+import { useRouter } from "next/navigation";
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
+
+import {
+  CRUDItem as Item,
+  TreeList,
+  useTree,
+  useTreeAction,
+} from "@acme/ui/components";
+import { useAction } from "@acme/ui/hooks";
+
+import { createDocument } from "~/actions";
 
 interface DocListProps {
   isMobile?: boolean;
 }
 
-const DocList = ({ isMobile }: DocListProps) => {
+const DocList = ({ isMobile: _isMobile }: DocListProps) => {
+  const _router = useRouter();
+  /** Docs */
+  const { isLoading } = useTree();
+  const { dispatch } = useTreeAction();
+  const onError = (e: string) => toast.error(e);
+  /** Create */
+  const { execute: create } = useAction(createDocument, {
+    onSuccess: (data) => {
+      const { id, title, parentId, isArchived, icon } = data;
+      dispatch({
+        type: "add",
+        payload: [{ id, title, parentId, isArchived, icon }],
+      });
+      toast.success(`Document Created: ${title}`);
+      // router.push(`/documents/${id}`);
+    },
+    onError,
+  });
+
   return (
     <>
       <div>
@@ -33,8 +65,28 @@ const DocList = ({ isMobile }: DocListProps) => {
             },
           }}
         />
+        <Item
+          label="New page"
+          icon={PlusCircle}
+          onClick={() => create({ title: "Untitled" })}
+        />
       </div>
-      <div className="mt-4">Doc Items</div>
+      <div className="mt-4">
+        {isLoading ? (
+          <>
+            <div className="mt-4">
+              {Array.from([0, 1, 0, 1, 1]).map((level, i) => (
+                <Item.Skeleton key={i} level={level} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <TreeList
+            parentId={null}
+            onAddItem={(parentId) => create({ title: "Untitled", parentId })}
+          />
+        )}
+      </div>
     </>
   );
 };
