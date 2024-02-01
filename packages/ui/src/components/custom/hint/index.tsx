d1c4d278ccc4c7620b7a8e67c4002507fca674be
steeeee0223 <mr.steven7@gmail.com>
2024-02-01 07:47:13 +0000

    🔧 `ui` Update components: button, hint

--- packages/ui/src/components/custom/hint/index.tsx
+++ packages/ui/src/components/custom/hint/index.tsx
@@ -10,23 +10,31 @@ import {
 export interface HintProps {
   children: ReactNode;
   description: string;
+  asChild?: boolean;
   side?: "left" | "right" | "top" | "bottom";
   sideOffset?: number;
+  align?: "start" | "center" | "end";
+  alignOffset?: number;
 }
 
 export const Hint = ({
   children,
   description,
+  asChild,
   side = "bottom",
   sideOffset,
+  align,
+  alignOffset,
 }: HintProps) => {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={0}>
-        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
         <TooltipContent
           sideOffset={sideOffset}
           side={side}
+          align={align}
+          alignOffset={alignOffset}
           className="max-w-[220px] break-words text-xs"
         >
           {description}
