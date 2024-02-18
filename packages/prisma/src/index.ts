08234ec7b71fb3c95e003230d9599485fab3a54d
steeeee0223 <mr.steven7@gmail.com>
2024-02-18 09:30:26 +0000

    🔧 `prisma` Added resolver package in `worxpace` app

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-var */
-import { PrismaClient as Auth } from ".prisma/auth";
-import { PrismaClient as Worxpace } from ".prisma/worxpace";
+import { PrismaClient as Auth } from ".prisma/client/auth";
+import { PrismaClient as Worxpace } from ".prisma/client/worxpace";
 
 declare global {
   var authClient: Auth | undefined;
@@ -29,5 +29,5 @@ export type {
   List,
   Subscription,
   User,
-} from ".prisma/worxpace";
-export { ROLE, ACTION, ENTITY_TYPE } from ".prisma/worxpace";
+} from ".prisma/client/worxpace";
+export { ROLE, ACTION, ENTITY_TYPE } from ".prisma/client/worxpace";
