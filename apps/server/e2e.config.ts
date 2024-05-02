68a391b273e63f5befad0adb348e4fd11a701b0e
Shih-Wen Yu <79098634+steeeee0223@users.noreply.github.com>
2024-05-02 15:44:23 +0800

    ✨ `server` Setup `nestjs` app (#46)
    * ✨ `server` Setup `nestjs` app
    
    * 🔧 `server` Fix format

---
+++ apps/server/e2e.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+
+import path from "path";
+import swc from "unplugin-swc";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: "./",
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
+    include: ["**/*.e2e-spec.ts"],
+  },
+  resolve: {
+    alias: { "~": path.resolve(__dirname, "./src") },
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      // module: { type: "es6" },
+    }),
+  ],
+});
