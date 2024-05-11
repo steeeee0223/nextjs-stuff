7e5ff184bfb94ec2193f669e9207bdc33c662b37
steeeee0223 <mr.steven7@gmail.com>
2024-05-11 06:46:01 +0000

    🔧 `storybook` Fix build

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"], //👈 Configures the static asset folder in Storybook
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
