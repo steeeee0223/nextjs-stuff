2dc729337da2b8f857d1fd8205f83f54d2bf0f3b
steeeee0223 <mr.steven7@gmail.com>
2024-06-13 11:19:05 +0000

    🔧 `ui` Fix settings panel: `useSettingsStore`

--- packages/ui/src/components/notion/settings-panel/use-settings.ts
+++ packages/ui/src/components/notion/settings-panel/use-settings.ts
@@ -7,32 +7,41 @@ import type { SettingsStore as Settings, UpdateSettings } from "./index.types";
 
 interface SettingsStore extends Settings {
   update: (...params: Parameters<UpdateSettings>) => void;
+  reset: () => void;
 }
+
+const initialSettings: Settings = {
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    imageUrl: "",
+  },
+  workspace: {
+    id: "",
+    name: "",
+    icon: { type: "lucide", name: "user" },
+    domain: "",
+  },
+  account: {
+    avatarUrl: "",
+    preferredName: "",
+    email: "",
+  },
+};
 
 export const useSettingsStore = create<SettingsStore>()(
   devtools(
     persist(
       (set) => ({
-        user: {
-          id: "",
-          name: "",
-          email: "",
-          imageUrl: "",
-        },
-        workspace: {
-          id: "",
-        },
-        account: {
-          avatarUrl: "",
-          preferredName: "",
-          email: "",
-        },
+        ...initialSettings,
         update: ({ user, workspace, account }) =>
           set((state) => ({
             user: { ...state.user, ...user },
             workspace: { ...state.workspace, ...workspace },
             account: { ...state.account, ...account },
           })),
+        reset: () => set(initialSettings),
       }),
       { name: "settings" },
     ),
